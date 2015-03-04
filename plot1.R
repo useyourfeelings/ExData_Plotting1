8a3864d78984639edc8249928345fa44d2f3e7fb
xc <raidercodebear@gmail.com>
2015-03-04 18:13:26 +0800

    i am afraid
    i am afraid

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@
 #I have noticed that the original data is well sorted by date and time in ascending order,
 #and we will only analyse the data of "2007-02-01" and "2007-02-02".
 #So here it is alright to read only 70000 rows from the start, for a performance reason.
-orignal_data<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", nrows = 70000)
+orignal_data<-read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
 #convert the Date column to Date type
 orignal_data["Date"] <- as.Date(orignal_data[["Date"]], "%d/%m/%Y")
